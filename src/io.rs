e11e16f243eb8c2c8dd399689fe6c495f01d2352
Iggorxz <ivvoloshin16@gmail.com>
2025-10-15 14:45:18 +0300

    Added text preprocessing function

--- src/io.rs
+++ src/io.rs
@@ -5,3 +5,9 @@ pub fn read_txt_file(file: &str) -> Result<String> {
     let text = fs::read_to_string(file)?;
     return Ok(text); 
 }
+
+pub fn format_ukrainian_text(text: String, alphabet: &str) -> String {
+    let mut formated_text = text.to_lowercase().replace("ґ", "г");
+    formated_text = formated_text.chars().filter(|ch| alphabet.contains(*ch)).collect();
+    return formated_text;
+}
