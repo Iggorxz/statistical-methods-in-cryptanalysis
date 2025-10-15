fa685fa5a50e9b263cd6167f8e9398bd872ac38d
Iggorxz <ivvoloshin16@gmail.com>
2025-10-15 21:06:32 +0300

    Added function for Criteria 2.1 (Common L-gram Threshold)

--- src/criterions.rs
+++ src/criterions.rs
@@ -9,3 +9,13 @@ pub fn criteria20(ukr_frequencies: &[(String, f64)], text_for_check: &String, nu
     let result = freq::existence_check(&ukr_top_frequencies, &lsequence_frequencies);
     return result;
 }
+
+pub fn criteria21(ukr_frequencies: &[(String, f64)], text_for_check: &String, num_top_lgrams: usize, l: usize, text_length: usize, limit: usize) -> bool {
+    let ukr_top_frequencies = freq::get_often_lgrams(ukr_frequencies, num_top_lgrams);
+    let lsequence = utils::get_subtext(text_for_check, text_length);
+    let lsequence_frequencies = freq::calculate_frequencies(&lsequence, l, None);
+    let common_lgrams = freq::intersection_of_sets(&lsequence_frequencies, &ukr_top_frequencies);
+    
+    let result = common_lgrams.len() >= limit;
+    return result;
+}
