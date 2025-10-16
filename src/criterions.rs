cf80536af8e2b2dc9fc2892d647d23308e026470
Iggorxz <ivvoloshin16@gmail.com>
2025-10-16 13:04:04 +0300

    Added function for Criteria 2.3 (Total Frequency Dominance)

--- src/criterions.rs
+++ src/criterions.rs
@@ -35,3 +35,14 @@ pub fn criteria22(ukr_frequencies: &[(String, f64)], text_for_check: &String, nu
 
     return true;
 }
+
+pub fn criteria23(ukr_frequencies: &[(String, f64)], text_for_check: &String, num_top_lgrams: usize, l: usize, text_length: usize) -> bool {
+    let ukr_top_frequencies = freq::get_often_lgrams(ukr_frequencies, num_top_lgrams);
+    let lsequence = utils::get_subtext(text_for_check, text_length);
+    let lsequence_frequencies = freq::calculate_frequencies(&lsequence, l, None);
+    let common_lgrams = freq::intersection_of_sets(&lsequence_frequencies, &ukr_top_frequencies);
+
+    let (kf, ff) = freq::calculate_common_frequencies(&ukr_top_frequencies, &common_lgrams);
+    let result = ff > kf;
+    return result;
+}
