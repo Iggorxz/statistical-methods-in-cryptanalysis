7467d67d5e0b14040dae5c1d794b2c06b291bcc7
Iggorxz <ivvoloshin16@gmail.com>
2025-10-19 14:07:48 +0300

    Refactor module structure

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod io;
+pub mod utils;
+pub mod freq;
+pub mod criterions;
+pub mod tests;
+pub mod config;
+
+pub use io::*;
+pub use utils::*;
+pub use freq::*;
+pub use criterions::*;
+pub use tests::*;
