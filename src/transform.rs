b1bf27b01931bf6c7e1a72f880fff8c48bc29784
Iggorxz <ivvoloshin16@gmail.com>
2025-10-22 21:40:02 +0300

    Optimize Vigenère cipher

--- src/transform.rs
+++ src/transform.rs
@@ -3,16 +3,15 @@ use crate::utils;
 
 pub fn vigenere_encryption(text: &String, key: &String, alphabet: &str) -> String {
     let mut result = String::new();
-    let key_indices: Vec<usize> = key.chars().filter_map(|k| alphabet.chars().position(|c| c == k)).collect();
-    let key_len = key.chars().count();
+    let alphabet_chars: Vec<char> = alphabet.chars().collect();
+    let key_indices: Vec<usize> = key.chars().filter_map(|k| alphabet_chars.iter().position(|&c| c == k)).collect();
+    let key_len = key_indices.len();
 
     for (i, ch) in text.chars().enumerate() {
-        if let Some(text_index) = alphabet.chars().position(|c| c == ch) {
+        if let Some(text_index) = alphabet_chars.iter().position(|&c| c == ch) {
             let key_index = key_indices[i % key_len];
-            let new_index = (text_index + key_index) % 32;
-            let new_char = alphabet.chars().nth(new_index)                
-            .expect("index is out of alphabet");
-            result.push(new_char); 
+            let new_index = (text_index + key_index) % alphabet_chars.len();
+            result.push(alphabet_chars[new_index]); 
         }
     }
 
