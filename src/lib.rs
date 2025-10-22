0b55333cda5bfceb0eca0d0a94f26af9848c310e
Iggorxz <ivvoloshin16@gmail.com>
2025-10-22 22:12:39 +0300

    Update module structure

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ pub mod freq;
 pub mod criterions;
 pub mod tests;
 pub mod config;
+pub mod transform;
 
 pub use io::*;
 pub use utils::*;
 pub use freq::*;
 pub use criterions::*;
 pub use tests::*;
+pub use transform::*;
