d56dc85898f1fa2503d035e301d7c05bb8be375d
Iggorxz <ivvoloshin16@gmail.com>
2025-10-22 19:45:43 +0300

    Implement Affine encryption algorithm

--- src/transform.rs
+++ src/transform.rs
@@ -1,3 +1,5 @@
+use num_integer::Integer;
+
 pub fn vigenere_encryption(text: &String, key: &String, alphabet: &str) -> String {
     let mut result = String::new();
     let key_indices: Vec<usize> = key.chars().filter_map(|k| alphabet.chars().position(|c| c == k)).collect();
@@ -11,6 +13,25 @@ pub fn vigenere_encryption(text: &String, key: &String, alphabet: &str) -> Strin
             .expect("index is out of alphabet");
             result.push(new_char); 
         }
+    }
+
+    return result;
+}
+
+pub fn affine_encryption(text: &String, key: (usize, usize), alphabet: &str) -> String {
+    let mut result = String::new();
+
+    if key.0.gcd(&32) != 1 {
+        panic!("a and m are not comprime");
+    }
+
+    for char in text.chars() {
+        let x: usize = alphabet.chars().position(|c| c == char)
+        .expect("Character not found in alphabet");
+        let y = (key.0 * x + key.1) % 32;
+        let new_char = alphabet.chars().nth(y)
+        .expect("index is out of alphabet");
+        result.push(new_char);
     }
 
     return result;
